963606d80ac325663851a4ddd60f1ed12c1b9aa3
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-10-21 07:07:33 -0300

    Estrutura tabela enderecos criada

---
+++ database/migrations/2021_10_21_100157_create_enderecos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id('end_cep');
+            $table->string('end_cidade');
+            $table->string('end_rua');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
