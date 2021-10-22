a8687f58b01cf8014d86341aa7950ecb31d53ae4
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-10-22 00:38:30 -0300

    Rotas create, list e api definidas

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\InitController::class, 'init'])->name('init');
+
+Route::post('/new_endereco/', [App\Http\Controllers\EnderecoController::class, 'create'])->name('create.endereco');
+Route::get('/lis_endereco/', [App\Http\Controllers\EnderecoController::class, 'list'])->name('list.endereco');
+Route::get('/cep/{cep}', [App\Http\Controllers\APIController::class, 'processCEP']);
