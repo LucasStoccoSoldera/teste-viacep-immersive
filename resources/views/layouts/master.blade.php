a017edae0226c2d9a4d6ebdc04cb794b92ab18ea
Lucas Solderagit config --global user.name Lucas <lucas.s.soldera2004@gmail.com>
2021-10-23 17:21:46 -0300

    CDN Personalizada DataTables

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"
         integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="//cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.3/b-2.0.1/sp-1.4.0/datatables.min.css"/>
+
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.3/b-2.0.1/sp-1.4.0/datatables.min.js"></script>
 
 </head>
 
